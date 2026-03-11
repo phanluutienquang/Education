e2dcd3bf3d6998199ba387d4d0856423575326b2
phanluutienquang <quangvip2081999@gmail.com>
2026-03-11 13:56:00 +0700

    remove EF dependency from domain layer

--- MyDomain/Entities/Course.cs
+++ MyDomain/Entities/Course.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
+
 
 namespace MyEducation.Domain.Entities;
 public partial class Course
