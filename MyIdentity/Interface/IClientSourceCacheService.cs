47474b1166474b742f0e9884b95920c837b8eb84
phanluutienquang <quangvip2081999@gmail.com>
2026-04-14 23:20:10 +0700

    clean: remove tracked files to apply gitignore

--- MyIdentity/Interface/IClientSourceCacheService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using MyEducation.MyIdentity.Models.Auth;
-
-public interface IClientSourceCacheService
-    {
-        Task<ClientSource?> GetClientAsync(string clientId);
-        Task InvalidateCacheAsync(string clientId);
-        Task<bool> IsClientValidAsync(string clientId);
-    }
--- MyIdentity/XClientSourceAuthenticationHandlerOptions.cs
+++
@@ -1,9 +0,0 @@
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.IdentityModel.Tokens;
-
-public class XClientSourceAuthenticationOptions : AuthenticationSchemeOptions
-{
-    public Func<string, SecurityToken, ClaimsPrincipal, Task<bool>> ClientValidator { get; set; } = (clientSource, token, principal) => Task.FromResult(false);
-    public string IssuerSigningKey { get; set; } = string.Empty;
-}
